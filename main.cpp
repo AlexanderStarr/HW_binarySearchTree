4393783863ab6efb97d24725e506994b816be8a5
Alexander Starr <AlexanderLStarr@gmail.com>
2013-07-20 10:13:15 -0500

    Began implementing binarySearchTree class.

--- main.cpp
+++ main.cpp
@@ -9,10 +9,23 @@ struct treeNode {
 	treeNode<T> (T init_data, treeNode<T> *left, treeNode<T> *right) {data = init_data; left_ptr=left; right_ptr=right;}
 };
 
+template <class T>
 class binarySearchTree {
-	
+	treeNode<T> *head;
+	int size;
+public:
+	binarySearchTree(){head=NULL; size=0;};
+	binarySearchTree(T data);
+};
+
+template <class T>
+binarySearchTree<T>::binarySearchTree(T data) {
+	treeNode<T> *tmp = new treeNode<T>(data, NULL, NULL);
+	head = tmp;
+	size = 1;
 };
 
 int main () {
+	binarySearchTree<int> myTree = binarySearchTree<int>(10);
     return 0;
 };
