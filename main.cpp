70abd9dfe27569f9ab7a27ea89c7be6d519e2648
Alexander Starr <AlexanderLStarr@gmail.com>
2013-07-20 09:48:18 -0500

    Created treeNode struct

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
+#include <string>
+using namespace std;
+
+template <class T>
+struct treeNode {
+	T data;
+	treeNode<T> *left_ptr, *right_ptr;
+	treeNode<T> (T init_data, treeNode<T> *left, treeNode<T> *right) {data = init_data; left_ptr=left; right_ptr=right;}
+};
+
+class binarySearchTree {
+	
+};
 
 int main () {
     return 0;
-}
+};
